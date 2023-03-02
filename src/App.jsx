673a82014d91f74c9a9c4a0904ab017dd52403c7
Arush Bansal <arush4bansal@gmail.com>
2023-03-02 23:16:41 +0530

    timeline

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import './App.css'
 import Navbar from "./components/navbar/navbar";
 import Footer from './components/Footer/footer';
+import Timeline from './components/timeline/timeline';
 
 function App() {
   return (
     <>
-    hello
        < Navbar />
-      <div style={{"min-height":"100vh"}}></div>
+       <div style={{"min-height":"100vh"}}></div>
+      <Timeline/>
       < Footer />
     </>
   );
---
+++ src/components/timeline/timeline.jsx
@@ -0,0 +1,52 @@
+import './timeline.css'
+
+export default function Timeline() {
+    return (
+        <div className="timeline">
+            <div className="timeline-heading">
+                <h1>Timeline</h1>
+            </div>
+            <div className="timeline-container scrollbar-hidden">
+                <div className="timeline-line"></div>
+                <div className="timeline-event-box">
+                    <div className="timeline-event timeline-event-top">
+                        <div>Registration Deadline</div>
+                        <div>18-March</div>
+                        <div className="timeline-dot timeline-dot-top"></div>
+                    </div>
+                    <div className="timeline-event timeline-event-bottom">
+                        <div>Round-1 Quiz</div>
+                        <div>18-March</div>
+                        <div className="timeline-dot timeline-dot-bottom"></div>
+                    </div>
+                    <div className="timeline-event timeline-event-top">
+                        <div>First Day of Kaizen</div>
+                        <div>19-March</div>
+                        <div className="timeline-dot timeline-dot-top"></div>
+                    </div>
+                    <div className="timeline-event timeline-event-bottom">
+                        <div>Inauguration Ceremony</div>
+                        <div>19-March</div>
+                        <div className="timeline-dot timeline-dot-bottom"></div>
+                    </div>
+                    <div className="timeline-event timeline-event-top">
+                        <div>Event Date</div>
+                        <div>19-March</div>
+                        <div className="timeline-dot timeline-dot-top"></div>
+                    </div>
+                    <div className="timeline-event timeline-event-bottom">
+                        <div>Registration Deadline</div>
+                        <div>20-March</div>
+                        <div className="timeline-dot timeline-dot-bottom"></div>
+                    </div>
+                    <div className="timeline-event timeline-event-top">
+                        <div>Finish Date</div>
+                        <div>20-April</div>
+                        <div className="timeline-dot timeline-dot-top"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
