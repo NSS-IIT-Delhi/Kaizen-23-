c74e2fc561d1d967b3c3470da980e7aaa4681404
Abhijeet <abhijeetgautam572@gmail.com>
2023-03-03 00:51:35 +0530

    add gallery carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 import Navbar from "./components/navbar/navbar";
-import Footer from './components/Footer/footer';
+import Footer from "./components/Footer/footer";
+import Carousel from "./components/Carousel/Carousel";
 
 function App() {
   return (
     <>
-    hello
-       < Navbar />
-      <div style={{"min-height":"100vh"}}></div>
-      < Footer />
+      hello
+      <Navbar />
+      <div style={{width:'100%', minHeight: "100vh", color: "white" }}>HOMEPAGE SECTION</div>
+      <div
+        style={{
+          width: "100%",
+          margin:'2rem 0',
+        }}
+        className="Carousel-container"
+      >
+        <Carousel />
+      </div>
+      <Footer />
     </>
   );
 }
---
+++ src/assets/images/images.js
@@ -0,0 +1,7 @@
+import slideshow1 from "./slideshow1.jpeg";
+import slideshow2 from "./slideshow2.jpeg";
+import slideshow3 from "./slideshow3.jpeg";
+import slideshow4 from "./slideshow4.jpeg";
+import slideshow5 from "./slideshow5.jpeg";
+
+export default { slideshow1, slideshow2, slideshow3, slideshow4, slideshow5 };
