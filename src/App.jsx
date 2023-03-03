21321788e6c0b9d2bd5468b5886db9927dbdc79d
Abhijeet <abhijeetgautam572@gmail.com>
2023-03-04 00:18:06 +0530

    create event section

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import "./App.css";
 import Navbar from "./components/navbar/navbar";
 import Footer from "./components/Footer/footer";
 import Carousel from "./components/Carousel/Carousel";
+import Events from "./components/Events/Events";
 
 function App() {
   return (
     <>
       hello
       <Navbar />
-      <div style={{width:'100%', minHeight: "100vh", color: "white" }}>HOMEPAGE SECTION</div>
+      {/* <div style={{ width: "100%", minHeight: "100vh", color: "white" }}>
+        HOMEPAGE SECTION
+      </div> */}
+      <Events/>
       <div
         style={{
           width: "100%",
-          margin:'2rem 0',
+          margin: "2rem 0",
         }}
         className="Carousel-container"
       >
