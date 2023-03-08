aede3074e89178432d2644a1f12cddba00ebe775
UrmilaSharma1 <ce1200264@iitd.ac.in>
2023-03-09 01:01:43 +0530

    css changes to events

--- src/components/EventPage/eventPage.jsx
+++ src/components/EventPage/eventPage.jsx
@@ -59,14 +59,14 @@ export default function Event() {
                   <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
                   <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
                 </svg>
-                {` ${time}, `}
+                {` ${time} `}
               </h4>
               <h4 class="px-3" style={{ "color": "white" }}>          
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="var(--primary-blue)" class="bi bi-pin-map" viewBox="0 0 16 16">
                   <path fill-rule="evenodd" d="M3.1 11.2a.5.5 0 0 1 .4-.2H6a.5.5 0 0 1 0 1H3.75L1.5 15h13l-2.25-3H10a.5.5 0 0 1 0-1h2.5a.5.5 0 0 1 .4.2l3 4a.5.5 0 0 1-.4.8H.5a.5.5 0 0 1-.4-.8l3-4z"/>
                   <path fill-rule="evenodd" d="M8 1a3 3 0 1 0 0 6 3 3 0 0 0 0-6zM4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999z"/>
                 </svg>
-                {` ${venue}, `}
+                {` ${venue} `}
               </h4>
             </div>
             <Tilt className="each-event pt-5 mt-5" style={{ backgroundColor: "transparent" }}>
@@ -83,7 +83,7 @@ export default function Event() {
             return(
               <div class="pt-5 text-center" style={{"color":"white"}}>
                 <div class="row py-4 px-3 justify-content-center text-center">
-                <h2 style={{"color":'var(--primary-blue)',"position":"relative","font":"Lobster"}}>{data.title}</h2>
+                <h2 className='family' style={{"color":'var(--primary-blue)',"position":"relative"}}>{data.title}</h2>
                     {data.data.map((card, key2) => { return (
                         <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12 p-4">
                             <Tilt className="Card p-4 mx-auto">
@@ -102,7 +102,7 @@ export default function Event() {
             return(
               <div class="pt-5 text-center" style={{"color":"white"}}>
                 <div class="row py-4 justify-content-center text-center">
-                <h2 style={{"color":'var(--primary-blue)',"position":"relative"}}>{data.title}</h2>
+                <h2 className='family'style={{"color":'var(--primary-blue)',"position":"relative"}}>{data.title}</h2>
                     {data.data.map((card, key2) => { return (
                         <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-12 p-4">
                             <Tilt className="Card p-4 mx-auto">
