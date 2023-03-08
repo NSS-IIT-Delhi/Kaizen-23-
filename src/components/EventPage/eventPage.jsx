ff8d8c012f1e070adcaa48a7e13b08ed23485c6e
UrmilaSharma1 <ce1200264@iitd.ac.in>
2023-03-08 20:01:35 +0530

    changes to registerbtn

--- src/components/EventPage/eventPage.jsx
+++ src/components/EventPage/eventPage.jsx
@@ -47,27 +47,27 @@ export default function Event() {
           <div className='reg col-xl-6 col-lg-6 col-md-5 col-sm-12 col-12 flex-column'>
             <div class="pb-5 pt-3"><RegisterButton id={id} /></div>
             <div class="py-4 px-5">
-              <h3 class="px-3" style={{ "color": "white" }}>
+              <h4 class="px-3" style={{ "color": "white" }}>
                 <svg width="24" height="24" fill="var(--primary-blue)" class="bi bi-calendar-event" viewBox="0 0 16 16">
                   <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"/>
                   <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
                 </svg>    
                 {` ${date}`}
-              </h3>
-              <h3 class="px-3" style={{ "color": "white" }}>          
+              </h4>
+              <h4 class="px-3" style={{ "color": "white" }}>          
                 <svg width="24" height="24" fill="var(--primary-blue)" class="bi bi-clock" viewBox="0 0 16 16">
                   <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
                   <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
                 </svg>
                 {` ${time}, `}
-              </h3>
-              <h3 class="px-3" style={{ "color": "white" }}>          
+              </h4>
+              <h4 class="px-3" style={{ "color": "white" }}>          
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="var(--primary-blue)" class="bi bi-pin-map" viewBox="0 0 16 16">
                   <path fill-rule="evenodd" d="M3.1 11.2a.5.5 0 0 1 .4-.2H6a.5.5 0 0 1 0 1H3.75L1.5 15h13l-2.25-3H10a.5.5 0 0 1 0-1h2.5a.5.5 0 0 1 .4.2l3 4a.5.5 0 0 1-.4.8H.5a.5.5 0 0 1-.4-.8l3-4z"/>
                   <path fill-rule="evenodd" d="M8 1a3 3 0 1 0 0 6 3 3 0 0 0 0-6zM4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999z"/>
                 </svg>
                 {` ${venue}, `}
-              </h3>
+              </h4>
             </div>
             <Tilt className="each-event pt-5 mt-5" style={{ backgroundColor: "transparent" }}>
               <EventDescription title={title} content={content} />
