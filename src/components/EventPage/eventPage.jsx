5396497891eab36007f9fad5b11ec300fa7c077a
KoRaghav <106992952+KoRaghav@users.noreply.github.com>
2023-03-15 11:40:46 +0530

    posters

--- src/components/EventPage/eventPage.jsx
+++ src/components/EventPage/eventPage.jsx
@@ -70,6 +70,7 @@ export default function Event() {
                 </svg>
                 {` ${time} `}
               </h4>
+              {venue ?
               <h4 class="px-3" style={{ "color": "white" }}>          
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="var(--primary-blue)" class="bi bi-pin-map" viewBox="0 0 16 16">
                   <path fill-rule="evenodd" d="M3.1 11.2a.5.5 0 0 1 .4-.2H6a.5.5 0 0 1 0 1H3.75L1.5 15h13l-2.25-3H10a.5.5 0 0 1 0-1h2.5a.5.5 0 0 1 .4.2l3 4a.5.5 0 0 1-.4.8H.5a.5.5 0 0 1-.4-.8l3-4z"/>
@@ -77,6 +78,7 @@ export default function Event() {
                 </svg>
                 {` ${venue} `}
               </h4>
+              :<></>}
             </div>
             <Tilt className="each-event pt-5 mt-5" style={{ backgroundColor: "transparent" }}>
               <EventDescription title={title} content={content} />
