89a1d71a45e9baa913a09072649ca1fd7bbde07c
UrmilaSharma1 <ce1200264@iitd.ac.in>
2023-03-19 21:37:28 +0530

    sponsers link added

--- src/components/Sponsers/Sponsers.jsx
+++ src/components/Sponsers/Sponsers.jsx
@@ -9,13 +9,13 @@ const Sponsers = () => {
         </h1>
         <div class="row justify-content-center m-auto">
           <div class="col-12 col-md-4 col-sm-6 mb-4  m-auto wid">
-            <img src="S1.jpg" class="img-fluid" style={{width:"300px",height:"200px"}} alt="Sponsor 1" />
+          <a href="https://thecoconutpeople.in/" target="_blank" rel="noopener noreferrer">   <img src="S1.jpg" class="img-fluid" style={{width:"300px",height:"200px"}} alt="Sponsor 1" /></a>
           </div>
           <div class="col-12 col-md-4 mb-4 col-sm-6  m-auto wid">
-            <img src="S2.jpg" class="img-fluid"style={{width:"300px",height:"200px"}} alt="Sponsor 2" />
+          <a href="https://thecampusmedia.com/" target="_blank" rel="noopener noreferrer">   <img src="S2.jpg" class="img-fluid"style={{width:"300px",height:"200px"}} alt="Sponsor 2" /></a>
           </div>
           <div class="col-12 col-md-4 mb-4 col-sm-6 m-auto wid">
-            <img src="S3.jpg" class="img-fluid"style={{width:"300px",height:"200px"}} alt="Sponsor 3" />
+          <a href="https://consciouscarma.com/" target="_blank" rel="noopener noreferrer">  <img src="S3.jpg" class="img-fluid"style={{width:"300px",height:"200px"}} alt="Sponsor 3" /></a>
           </div>
         </div>
 
